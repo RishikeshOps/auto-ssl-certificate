2100b6b9cb59ef8fd901383c25c573f63f87a3a1
Ubuntu <ubuntu@ip-172-31-86-24.ec2.internal>
2023-04-11 08:01:51 +0000

    initial commit

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(sachin1.rushikesh.me www.sachin1.rushikesh.me)
+domains=(example.me www.example.me)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="rishikeshmashidkar@gmail.com" # Adding a valid address is strongly recommended
