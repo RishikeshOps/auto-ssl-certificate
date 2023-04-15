4143ccb7a002992735fb2505628b567763d1db03
Rushikesh Mashidkar <rishikeshmashidkar@gmail.com>
2023-04-15 11:34:20 +0530

    Update init.sh

--- init.sh
+++ init.sh
@@ -10,7 +10,7 @@ docker-compose up -d
 domains=(example.me www.example.me)
 rsa_key_size=4096
 data_path="./data/certbot"
-email="rishikeshmashidkar@gmail.com" # Adding a valid address is strongly recommended
+email="example@gmail.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
